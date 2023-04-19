29b8ca32b88d95703f80d171ce15b42605012a60
Zenafey <118455214+yoou3-cyber@users.noreply.github.com>
2023-04-19 06:03:19 -0700

    Update kandinsky.py
    - added aspect ratio option
    - version.predict replaced with replica.run

--- kandinsky.py
+++ kandinsky.py
@@ -20,21 +20,36 @@ async def on_ready():
     print("-----------------------------------")
     print(f"{Fore.GREEN}       loggined as {bot.user.display_name}       {Fore.RESET}")
     print("-----------------------------------")
-#define slash command kandinsky
+    
+    
 @bot.slash_command(name="kandinsky", description="Create images with Kandinsky2.1")
 @option("prompt", description="Enter a prompt for image")
-@option("steps", description="Enter steps value(1-500)", min_value=1, max_value=500, default=50) #define options for slash command
-@option("scale", description="Enter scale value(1-20)", min_value=1, max_value=20, default=4)
-async def kandinsky(ctx, prompt:str, steps:int, scale:float): #function of slash command
-    embed1 = discord.Embed(title="Job in progress...", color=0xff0000,
-                           description=f"```propmpt:{prompt}\nsteps:{steps}\nscale:{scale}```")
-    msg = await ctx.respond(embed=embed1)#send text that will be used in generation
-    model = replicate.models.get("cjwbw/kandinsky-2")# get replicate model
-    version = model.versions.get("65a15f6e3c538ee4adf5142411455308926714f7d3f5c940d9f7bc519e0e5c1a")# get version of replicate model
-    image = version.predict(prompt=prompt, num_inference_steps=steps, guidance_scale=scale) # make a predict and get answer as url of image
-    embed2 = discord.Embed(title="Kandinsky Art", color=0xff0000,
-                           description=f"```prompt:{prompt}\nsteps:{steps}\nscale:{scale}```")
-    embed2.set_image(url=image)#set url of embed
-    await msg.edit_original_response(content=ctx.author.mention, embed=embed2) #edit first response with adding generated image
+@option("aspect_ratio", desciption="Choose aspect ratio for image(default is 1:1)", choices=["1:1", "2:3", "3:2"], default="1:1")
+@option("steps", description="Enter steps value(1-50)", min_value=1, max_value=500, default=50)
+@option("scale", description="Enter scale value(1-10)", min_value=1, max_value=20, default=4)
+async def kandinsky(ctx, prompt:str, aspect_ratio:str, steps:int, scale:float):
+    text = f"```propmpt:{prompt}\naspect_ratio:{aspect_ratio}\nsteps:{steps}\nscale:{scale}```"
+    embed1 = discord.Embed(title=" Job in progress...", color=discord.Color.blue(), description=text)
+    msg = await ctx.respond(embed=embed1)
+    if aspect_ratio == "1:1":
+        width = 512
+        height = 512
+    elif aspect_ratio == "2:3":
+        width = 512
+        height = 768
+    elif aspect_ratio == "3:2":
+        width = 768
+        height = 512
+    inputs = {
+        "prompt": prompt,
+        "num_inference_steps": steps,
+        "guidance_scale": scale,
+        "width": width,
+        "height": height
+    }
+    image = replicate.run("ai-forever/kandinsky-2:601eea49d49003e6ea75a11527209c4f510a93e2112c969d548fbb45b9c4f19f", input=inputs)
+    embed2 = discord.Embed(title="<:draw:1090244543280066560> Kandinsky Art", color=discord.Color.blue(), description=text)
+    embed2.set_image(url=image)
+    await msg.edit_original_response(content=ctx.author.mention, embed=embed2)
 
 bot.run(ds_token) #run the bot
